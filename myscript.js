4fab943336baa64935575a5d26c586e236d330d2
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-09-01 09:09:32 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-	$("h1").click(function() {
-		$("img").slideToggle("slow");
-		$("p").css("color","blue")
+	$("#button").click(function() {
+		$("img").slideDown("slow");
 	});
 });
