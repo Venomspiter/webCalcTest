60c287ab753a46e9ee5ea926329f791244468fe3
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-09-07 09:54:11 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,55 @@
 $(document).ready(function() {
+	var x = 0;
 	$("p").click(function() {
-		$("img").slideDown("slow");
+		$("#Q1").slideDown("slow");
 		$("h2").toggle();
 		$("p").hide();
 	});
+	$("h2").click(function() {
+		if (x<1){
+		$("h3").fadeIn();
+		$("h4").fadeIn();
+		$("h2").toggle();
+		x+=1
+		}
+		if (x>4){
+		$("h5").fadeIn();
+		$("h4").fadeIn();
+		$("h2").toggle();
+		}
+		});
+		$("h4").click(function() {
+			if(x<4){
+			$("h4").fadeOut();
+			$("h2").toggle();
+			$("h3").hide();
+			$("#Q1").fadeOut("slow");
+			$("#Q2").slideDown("slow");
+			x+=2
+			}
+			if(x>4 && x<7){
+				$("h4").fadeOut();
+			$("h2").fadeIn();
+			$("h6").hide();
+			$("#Q2").fadeOut("slow");
+			$("#Q3").slideDown("slow");
+			x+=1
+			}
+			if(x>6){
+				$("div").fadeIn();
+				$("h6").hide();
+				$("h4").hide();
+				$("h2").hide();
+				$("h5").hide();
+				$("#Q3").fadeOut("slow");
+				$("form").fadeIn();
+			}
+		});
+	$("h2").click(function() {
+		if(x>1 && x<4){
+		$("h2").toggle();
+		$("h6").fadeIn();
+		$("h4").fadeIn();
+		}
+		});
 });
